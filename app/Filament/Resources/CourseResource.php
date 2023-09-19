e2b09a00581db5187a6a00055e816181326ee036
mattot-the-builder <135392742+mattot-the-builder@users.noreply.github.com>
2023-09-19 17:50:23 +0800

    Added course

---
+++ app/Filament/Resources/CourseResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CourseResource\Pages;
+use App\Filament\Resources\CourseResource\RelationManagers;
+use App\Models\Course;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CourseResource extends Resource
+{
+    protected static ?string $model = Course::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-academic-cap';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\TextInput::make('fee')
+                    ->required()
+                    ->prefix('RM')
+                    ->numeric(),
+                Forms\Components\RichEditor::make('contents')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\Section::make('Date and Time')
+                    ->schema([
+                        Forms\Components\DatePicker::make('date')
+                            ->required(),
+                        Forms\Components\TimePicker::make('started_at')
+                            ->required(),
+                        Forms\Components\TimePicker::make('ended_at')
+                            ->required(),
+                    ])->columns(3),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('started_at'),
+                Tables\Columns\TextColumn::make('ended_at'),
+                Tables\Columns\TextColumn::make('fee')
+                    ->numeric()
+                    ->money('myr')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageCourses::route('/'),
+        ];
+    }
+}
