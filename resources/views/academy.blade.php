89dc0f5ab3a8f026ae28cfa6f088522d7d9a36b2
mattot-the-builder <135392742+mattot-the-builder@users.noreply.github.com>
2023-10-01 16:10:40 +0800

    Fixed download button to link

--- resources/views/academy.blade.php
+++ resources/views/academy.blade.php
@@ -92,10 +92,10 @@
                         class="mx-auto text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                         Hide Course
                     </button>
-                    <button type="button"
+                    <a href="/download-list-courses" type="button"
                         class="mx-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Download List of Courses
-                    </button>
+                    </a>
                     <div x-show="show">
                         <livewire:training-course />
                     </div>
@@ -220,10 +220,10 @@
                     <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                     <ul class="text-gray-500 dark:text-gray-400 font-medium">
                         <li class="mb-4">
-                            <a href="https://loremflickr.com/2500/1600/funny" class="hover:underline">Engineering</a>
+                            <a href="/" class="hover:underline">Engineering</a>
                         </li>
                         <li>
-                            <a href="https://loremflickr.com/2500/1600/funny" class="hover:underline">Academy</a>
+                            <a href="{{ route('academy') }}" class="hover:underline">Academy</a>
                         </li>
                     </ul>
                 </div>
